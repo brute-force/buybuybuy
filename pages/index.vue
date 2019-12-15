f83cc8012169bfb4a47cd64f1312290ab60f9075
Reuel Alvarez <reuel.alvarez@gmail.com>
2019-12-15 18:22:07 -0500

    search ticker page w/ autocomplete

--- pages/index.vue
+++ pages/index.vue
@@ -9,22 +9,61 @@
       sm8
       md6
     >
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
+      <div class="mt-6">
+        <v-autocomplete
+          v-model="ticker"
+          :items="items"
+          :loading="isLoading"
+          :search-input.sync="search"
+          item-text="1. symbol"
+          item-value="2. name"
+          hide-no-data
+          label="Ticker"
+          placeholder="Start typing to Search"
+          prepend-icon="mdi-database-search"
+          return-object
+          clearable
+          @input="go"
+        />
       </div>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue';
-import VuetifyLogo from '~/components/VuetifyLogo.vue';
+import debounce from 'debounce';
 
 export default {
-  components: {
-    Logo,
-    VuetifyLogo
+  data () {
+    return {
+      isLoading: false,
+      ticker: null,
+      items: [],
+      search: ''
+    };
+  },
+  watch: {
+    search (val) {
+      if (val) {
+        debounce(this.doSearch, 200)(val, this);
+      }
+    }
+  },
+  methods: {
+    async doSearch (val) {
+      try {
+        this.isLoading = true;
+        const { data: { bestMatches } } = await this.$axios.get(`/query?function=SYMBOL_SEARCH&keywords=${val}&apikey=${process.env.apiKeyAlphaVantage}`);
+
+        this.items = bestMatches;
+        this.isLoading = false;
+      } catch (err) {
+        this.error(err);
+      }
+    },
+    go (val) {
+      this.$router.push({ path: `/${val['1. symbol']}` });
+    }
   }
 };
 </script>
